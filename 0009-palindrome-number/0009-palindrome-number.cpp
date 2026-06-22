7e0d0a63a39747e3878e5f5c169322ad0ad42c22
Niharikapatel05 <niharikapatel441@gmail.com>
2026-06-22 11:11:38 +0530

    Time: 0 ms (100%), Space: 8.5 MB (65.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        long long rev = 0;
+        int n = x;
+        while(n > 0){
+            int rem = n%10;
+            rev = rev*10 + rem;
+            n = n/10;
+        }
+
+        return rev == x;
+    }
+
+};
